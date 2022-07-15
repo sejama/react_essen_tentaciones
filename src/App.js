e07db0b3c72b5ef0471989201b096c9c82468a59
Sebastian Maidana <seba_maidana_3@hotmail.com>
2022-07-15 17:45:31 -0300

    Iniciando el proyecto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editamos <code>src/App.js</code> gracias al profe y los tutores!!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bienvenido a React gracias a CoderHouse
         </a>
       </header>
     </div>
