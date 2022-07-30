27fc4c5a537f540360252f2bef8c4bfd2bdd46a5
Sebastian Maidana <seba_maidana_3@hotmail.com>
2022-07-30 20:09:07 -0300

    Desafio 3

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -2,7 +2,7 @@
 function ItemListContainer({nombre}){
     return (
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-            <h1>{nombre}</h1>
+            <h1 style={{color: 'blue'}}>{nombre}</h1>
         </div>
     );
 }
