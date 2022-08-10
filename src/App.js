d6bff33a9d4a7f3f6030bb5ea4ab0ec070e943be
Sebastian Maidana <seba_maidana_3@hotmail.com>
2022-08-10 19:26:01 -0300

    Desafio 7

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import  'bootstrap/dist/css/bootstrap.min.css' ;
 import NavBar from './componentes/NavBar';
 import ItemListContainer from './componentes/ItemListContainer';
 import ItemDetailContainer from './componentes/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <ItemDetailContainer /> 
-      <br /><br />
-      <ItemListContainer nombre="Titulo del ItemListContainer"/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" component={<ItemListContainer nombre="Titulo del ItemListContainer"/>} />
+          <Route path="/detalle" component={<ItemDetailContainer />} />
+          <NavBar  />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
